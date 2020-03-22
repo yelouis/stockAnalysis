d9f0f0a17a81e6e9e1e97bf3892121649f392bdf
Louis Ye <yel@carleton.edu>
2020-03-22 12:18:17 -0700

    Added configKeys.py to clean up code. Also cleaned up the itterator
    Do not push any changes in the binaries because our systems might to be compatable. Text or call me if that is confusing.

--- stock_importing.py
+++ stock_importing.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 # Import the yfinance. If you get module not found error the run !pip install yfiannce from your Jupyter notebook
 import yfinance as yf
 
+#This file will keep track of all the moving variables and we can slowly add to that file
+import configKeys
+
 # Get the data of the stock AAPL
 #data = yf.download('AAPL','2016-01-01','2018-01-01')
 
@@ -22,13 +25,22 @@ df = pd.read_csv("companylist.csv", low_memory=False)
 stockTickers = df.Symbol
 
 masterStockList = []
-startPull = '2016-01-01'
-endPull = '2019-01-01'
-csvIndex = 1
-for stockName in stockTickers:
-    stockData = yf.download(stockName, startPull, endPull)
-    stock = Stock(stockName, stockData, df.loc[csvIndex, ['Sector']], df.loc[csvIndex, ['IPOyear']], startPull+endPull)
-    masterStockList.append(stock)
+# csvIndex = 1
+# for stockName in stockTickers:
+#     stockData = yf.download(stockName, configKeys.STARTPULL, configKeys.ENDPULL)
+#     stock = Stock(stockName, stockData, df.loc[csvIndex, ['Sector']], df.loc[csvIndex, ['IPOyear']], configKeys.STARTPULL+configKeys.ENDPULL)
+#     masterStockList.append(stock)
+
+
+for ind in df.index:
+    if(df['Sector'][ind] == configKeys.SECTORLIST):
+        stockData = yf.download(df['Symbol'][ind], configKeys.STARTPULL, configKeys.ENDPULL)
+        stock = Stock(df['Symbol'][ind], stockData, df['Sector'][ind], df['IPOyear'][ind], configKeys.STARTPULL+configKeys.ENDPULL)
+        masterStockList.append(stock)
+
+
+
+
 
 for i in masterStockList:
     print(i.symbol)

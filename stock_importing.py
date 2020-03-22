653538954243bae6dcd7972be2e6127559cf032f
diiannic <diiannic@carleton.edu>
2020-03-22 14:07:57 -0400

    Updated stock importing to get multiple stocks from the stock ticker csv

--- stock_importing.py
+++ stock_importing.py
@@ -5,8 +5,30 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 
 # Get the data of the stock AAPL
-data = yf.download('AAPL','2016-01-01','2018-01-01')
+#data = yf.download('AAPL','2016-01-01','2018-01-01')
 
 # Plot the close price of the AAPL
-data.Close.plot()
-plt.show()
+#data.Close.plot()
+#plt.show()
+
+import pandas as pd
+import collections
+import copy
+
+Stock = collections.namedtuple('Stock', ['symbol', 'price', 'sector', 'IPOyear', "dates"])
+
+df = pd.read_csv("companylist.csv", low_memory=False)
+
+stockTickers = df.Symbol
+
+masterStockList = []
+startPull = '2016-01-01'
+endPull = '2019-01-01'
+csvIndex = 1
+for stockName in stockTickers:
+    stockData = yf.download(stockName, startPull, endPull)
+    stock = Stock(stockName, stockData, df.loc[csvIndex, ['Sector']], df.loc[csvIndex, ['IPOyear']], startPull+endPull)
+    masterStockList.append(stock)
+
+for i in masterStockList:
+    print(i.symbol)

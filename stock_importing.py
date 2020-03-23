7f12401bcd5f648f58f7c943020ce5a654aa17c0
Louis Ye <yel@carleton.edu>
2020-03-22 18:27:33 -0700

    deleting some commented out code

--- stock_importing.py
+++ stock_importing.py
@@ -7,7 +7,6 @@ import yfinance as yf
 #This file will keep track of all the moving variables and we can slowly add to that file
 import configKeys
 
-#<<<<<<< HEAD
 # Get the data of the stock AAPL
 #data = yf.download('AAPL','2016-01-01','2018-01-01')
 
@@ -26,12 +25,6 @@ df = pd.read_csv("companylist.csv", low_memory=False)
 stockTickers = df.Symbol
 
 masterStockList = []
-# csvIndex = 1
-# for stockName in stockTickers:
-#     stockData = yf.download(stockName, configKeys.STARTPULL, configKeys.ENDPULL)
-#     stock = Stock(stockName, stockData, df.loc[csvIndex, ['Sector']], df.loc[csvIndex, ['IPOyear']], configKeys.STARTPULL+configKeys.ENDPULL)
-#     masterStockList.append(stock)
-
 
 for ind in df.index:
     # Have an if statement in place in case if we don't want to pull every stock because there are a lot of stocks

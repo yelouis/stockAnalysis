429e152d8d124f31e3f8ae56d481fb45c3b9013c
Louis Ye <yel@carleton.edu>
2020-03-29 19:51:47 -0700

    Got an auto creation of successful pull file. Realized that I spelled it wrong

--- Stock_Prices_Daily/stock_importing.py
+++ Stock_Prices_Daily/stock_importing.py
@@ -18,6 +18,7 @@ import pandas as pd
 import collections
 import copy
 import os
+import csv
 
 from pathlib import Path
 
@@ -29,6 +30,8 @@ stockTickers = df.Symbol
 
 data_folder = Path("Daily/")
 
+sucessfulPulls = [["Symbol", "Sector"]]
+
 for ind in df.index:
     # Have an if statement in place in case if we don't want to pull every stock because there are a lot of stocks
     # Program takes a long time to run if we have to webscrape every stock each time we run
@@ -39,6 +42,8 @@ for ind in df.index:
     # https://pypi.org/project/yfinance/
     stockData = yf.download(df['Symbol'][ind], start=configKeys.STARTPULL, end=configKeys.ENDPULL)
 
+    if stockData.empty == False:
+        sucessfulPulls.append([df['Symbol'][ind], df['Sector'][ind]])
 
 
     # If there's something that's been loaded into stockData, then the length is no longer 0
@@ -47,6 +52,10 @@ for ind in df.index:
         stockData.to_csv(os.path.join(data_folder, df['Symbol'][ind]+'Daily.csv'))
 
 
+#Creating a sucessful file that includes stock tickers and sectors
+with open("sucessfulPulls.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(sucessfulPulls)
 
 #USE THIS CODE WHEN READING THE CSV's INTO OTHER PYTHON FILES!!!
 '''

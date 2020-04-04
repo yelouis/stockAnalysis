794c67b7a92d403595aeba9d6d09bc3dab9390e0
diiannic <diiannic@carleton.edu>
2020-04-04 17:01:13 -0400

    updated the binning system

--- Miners/data_analysis.py
+++ Miners/data_analysis.py
@@ -17,10 +17,13 @@ import copy
 import os
 import csv
 import datetime
+from datetime import timedelta
+import statistics
+import numpy as np
 
 from pathlib import Path
 
-def lassoRegressionImplement(stockDF, alpha):
+def lassoRegressionImplement(df, alpha):
     '''
     stockA, stockB, stockC
     xValues = [[stockA.vol, stockB.vol, stockC.vol],[stockA.volit, stockB.volit, stockC.volit],[stockA.lowPrice, stockB.lowPrice, stockC.lowPrice]]
@@ -33,38 +36,170 @@ def lassoRegressionImplement(stockDF, alpha):
     xValues = [[stockA.volW1, stockA.volW2, stockA.volW3],[stockA.volitW1, stockA.volitW2, stockA.volitW3]]
     yValues = [stockA.highestW2, stockA.highestW3, stockA.highestW4]
     '''
+    xValues = []
+    yValues = []
+    xValueNames = []
 
 
-    ##############################################################################
     '''
-    This piece of code breaks up the stocks into weeks
+    This bins everything into weeks
     '''
+
+    startBinDatetime, endBinDatetime = datetime.datetime.strptime(configKeys.STARTPULL, '%Y-%m-%d'), datetime.datetime.strptime(configKeys.ENDPULL, '%Y-%m-%d')
+
+    countDatetime = startBinDatetime
+    bins = []
+    datetimeBin = []
+
+    while (countDatetime < endBinDatetime): # while the count time is not at the last week in the sequence
+        datetimeBin = [countDatetime, []] # could add more [] lists if you want to add volume and other criteria
+        bins.append(datetimeBin)
+        countDatetime = countDatetime + timedelta(days=7)
+
+
+    #This first puts the y value into the bins list. This is to give us easy access when trying to move it to the yValues list
+
+    head = 0
     stockWeek = []
-    specficWeek = []
+
+    stockDF = pd.read_csv(os.path.join(Path(configKeys.DATA_FOLDER), df['Y Value'][0]+'.csv'), low_memory=False)
+
     for ind in stockDF.index:
+
         stockDF.at[ind, 'Date'] = datetime.datetime.strptime(stockDF['Date'][ind], '%Y-%m-%d')
 
-        if ind == 0:
-            specficWeek.append([stockDF['Date'][ind], stockDF['Open'][ind], stockDF['High'][ind], stockDF['Low'][ind], stockDF['Close'][ind], stockDF['Adj Close'][ind], stockDF['Volume'][ind]])
+        if (stockDF['Date'][ind] - bins[head][0]).days <= 7:
+            # We know that we've got the right index
+            stockWeek.append(stockDF['Close'][ind])
+
         else:
-            if (stockDF['Date'][ind] - stockDF['Date'][ind-1]).days >= 2:
-                stockWeek.append(specficWeek)
-                specficWeek = []
-            specficWeek.append([stockDF['Date'][ind], stockDF['Open'][ind], stockDF['High'][ind], stockDF['Low'][ind], stockDF['Close'][ind], stockDF['Adj Close'][ind], stockDF['Volume'][ind]])
+            while (stockDF['Date'][ind] - bins[head][0]).days > 7:
+                if (len(stockWeek) == 0):
+                    bins[head][1].append(None)
+                else:
+                    bins[head][1].append(statistics.mean(stockWeek))
+                head += 1
+                stockWeek = []
+
+    # We have to do this one more time to get the values from the last week
+    if (len(stockWeek) == 0):
+        bins[head][1].append(None)
+    else:
+        bins[head][1].append(statistics.mean(stockWeek))
+
+    #This for loop then collects all the xValue data into the bins
+    for stockNameInd in df['Lagged X Value'].index:
+
+        head = 0
+        stockWeek = []
+
+        stockDF = pd.read_csv(os.path.join(Path(configKeys.DATA_FOLDER), df['Lagged X Value'][stockNameInd]+'.csv'), low_memory=False)
+
+        for ind in stockDF.index:
+
+            stockDF.at[ind, 'Date'] = datetime.datetime.strptime(stockDF['Date'][ind], '%Y-%m-%d')
+
+            if (stockDF['Date'][ind] - bins[head][0]).days <= 7:
+                # We know that we've got the right index
+                stockWeek.append(stockDF['Close'][ind])
+
+            else:
+                while (stockDF['Date'][ind] - bins[head][0]).days > 7:
+                    if (len(stockWeek) == 0):
+                        bins[head][1].append(None)
+                    else:
+                        bins[head][1].append(statistics.mean(stockWeek))
+                    head += 1
+                    stockWeek = []
+
+        # We have to do this one more time to get the values from the last week
+        if (len(stockWeek) == 0):
+            bins[head][1].append(None)
+        else:
+            bins[head][1].append(statistics.mean(stockWeek))
+
+    print(bins)
+    quit()
+
+    '''
+    INVERT THE bins LIST!!!!!
+    '''
+
+    #xValues = bins[]
+
+
     ##############################################################################
+    '''
+    This piece of code breaks up X value stocks into weeks
+    '''
+
+    for stockNameInd in df['Lagged X Value'].index:
+
+        stockDays = []
+        avgStockWeek = []
+        varianceStockWeek = []
+
+        stockDF = pd.read_csv(os.path.join(Path(configKeys.DATA_FOLDER), df['Lagged X Value'][stockNameInd]+'.csv'), low_memory=False)
+
+        for ind in stockDF.index:
+
+            stockDF.at[ind, 'Date'] = datetime.datetime.strptime(stockDF['Date'][ind], '%Y-%m-%d')
+
+            if ind == 0:
+                stockDays.append(stockDF['Close'][ind]) #stockDF['Volume'][ind])
+            else:
+                if (stockDF['Date'][ind] - stockDF['Date'][ind-1]).days >= 2 and len(stockDays) > 1:
+                    avgStockWeek.append(statistics.mean(stockDays))
+                    varianceStockWeek.append(statistics.variance(stockDays))
+                    stockDays = []
+                elif ((stockDF['Date'][ind] - stockDF['Date'][ind-1]).days >= 2):
+                    stockDays = []
+                stockDays.append(stockDF['Close'][ind])
+        if len(stockDays) > 1:
+            avgStockWeek.append(statistics.mean(stockDays))
+            varianceStockWeek.append(statistics.variance(stockDays))
+
+        # This will set the first x values to be the lagged version of the y value (price of the stock in question)
+        xValues.append(avgStockWeek[:-1])
+        xValues.append(varianceStockWeek[:-1])
+
+        xValueNames.append(df['Lagged X Value'][stockNameInd] + ' average')
+        xValueNames.append(df['Lagged X Value'][stockNameInd] + ' variance')
 
 
 
+
+
+    #This will then break the Y value stock into weeks and add it to yValues and its lag to xValues
+
+
+    yValues = avgStockWeek[1:]
+
+    print([len(val) for val in xValues])
+    quit()
+
+    ##############################################################################
 
 
     '''
     Standardize the x values
     '''
+
+
+
+
+
     from sklearn.preprocessing import StandardScaler
     scalerX = StandardScaler()
     scalerX.fit(xValues)
     xValues = scalerX.transform(xValues)
 
+    '''
+    Regarding standardizing the yValues. We might want to think about standardizing
+    it in comparison to the previous week's highs rather than standardizing it to each
+    other. That way we would be predicting how much higher or lower the high of next
+    week's stock is going to be in relation to the previous week's highs
+    '''
     # standardizedYValues = []
     # for target in yValues:
     #     standardizedYValues.append([target])
@@ -125,15 +260,9 @@ def lassoRegressionImplement(stockDF, alpha):
 This is the part that actually runs the code
 '''
 
-df = pd.read_csv("successfulPulls.csv", low_memory=False)
-
-allStock = {}
-
-for ind in df.index:
-    stockDF = pd.read_csv(os.path.join(Path(configKeys.DATA_FOLDER), df['Symbol'][ind]+'Daily.csv'), low_memory=False)
-    allStock[df['Symbol'][ind]] = stockDF
+df = pd.read_csv("XYValues.csv", low_memory=False)
 
 alpha = 0.1
 for counter in range(10):
-    lassoRegressionImplement(allStock['JNJ'], alpha)
+    lassoRegressionImplement(df, alpha)
     alpha += 0.1

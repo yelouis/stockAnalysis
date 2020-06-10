31060f5aa3baab7591ecc31e8869dea5faee1bcf
Nathaniel <nveimau1@swarthmore.edu>
2020-06-10 00:52:35 -0700

    CSV for all tickers

--- Stock_Prices_Daily/sn.py
+++ Stock_Prices_Daily/sn.py
@@ -19,28 +19,16 @@ import copy
 import os
 import csv
 import math
-
+import time
 
 from pathlib import Path
 
-Stock = collections.namedtuple('Stock', ['symbol', 'price', 'sector', 'IPOyear', "dates"])
+#intrinDF is a dataframe reading in all the info from companylist.csv
+#the skiprows avoids the second line of the csv
+intrinDF = pd.read_csv("companylist.csv", low_memory=False, skiprows = range(1,1))
 
-#df = pd.read_csv("companylist.csv", low_memory=False)
-#This reads one stock into a dataframe which is : American Express for testing purposes
-intrinDF = pd.read_csv("companylist.csv", low_memory=False, header = 0, skiprows = range(1, 267), nrows = 2)
-'''
-print("Size")
-print(intrinDF.size) #14 = Represents Header Values, and ACB row values
-print("Indeces")
-print(intrinDF.index) # RangeIndex(start=0, stop=2, step=1)
-print("Columns")
-print(intrinDF.columns) # Index(['Symbol', 'Name', 'Unnamed: 2', 'Unnamed: 3', 'IPOyear', 'Sector', 'industry'], dtype='object')
-'''
-
-#This should be ACB
-intrinTicker = intrinDF.at[1, "Symbol"]
-if intrinTicker != "AXP":
-    print("Ticker set incorrectly")
+#intrinTickers holds all the tickers from the companylist.csv
+intrinTickers = intrinDF["Symbol"]
 
 #.SecurityApi() is used to declare we are searching for stocks (Alternate options are for example OptionsApi, MunicipalityApi)
 security_api = intrin.SecurityApi()
@@ -48,72 +36,62 @@ security_api = intrin.SecurityApi()
 firstIndex = datetime.datetime.strptime(configKeys.STARTPULL, '%Y-%m-%d') + timedelta(days=1) # date | Return prices on or after the date (optional)
 lastIndex = datetime.datetime.strptime(configKeys.ENDPULL, '%Y-%m-%d') - timedelta(days=1) # date | Return prices on or before the date (optional)
 
-#This calculates the number of weeks needed to look back as defined by the dates in configKeys and using that number as the page_size (number of datapoints)
+#This calculates the number of days needed to look back as defined by the dates in configKeys and using that number as the page_size (number of requests)
 necessaryPageSize = 0;
-numWeeks = abs((firstIndex - lastIndex).days)/7
-necessaryPageSize = math.ceil(numWeeks)
+numDays = abs((firstIndex - lastIndex).days)
+necessaryPageSize = math.ceil(numDays)
 
-print("necessaryPageSize should be 209: " + str(necessaryPageSize))
+#holds the stocks that have data between our start and end dates
+successfulPulls = [["Symbol", "Sector"]]
 
-try:
-    #stockDataSummary is a list of StockPriceSummary objects, documentation for it can be found here: https://docs.intrinio.com/documentation/python/get_security_stock_prices_v2
-    #NOTE: The date will always represent the last day in the period (E.g. End of the week)
-    stockDataSummary = security_api.get_security_stock_prices(intrinTicker, start_date=firstIndex, end_date=lastIndex, frequency="weekly", page_size=necessaryPageSize)
-    #extract data
+for intrinTicker in intrinTickers:
+    tickerRowIndex = 0
+    #Result =  boolean dataframe with True at the position where the the intrinTicker is in intrinDF
+    result = intrinDF.isin([intrinTicker])
+    #By fetching the index of the row where the boolean dataframe has the value True
+    row = result["Symbol"][result["Symbol"] == True].index
+    tickerRowIndex = row
 
-except ApiException as e:
-    print("Exception when calling SecurityApi->get_security_stock_prices: %s\r\n" % e)
+    #initialization of a dataframe to help us keep track of stock price data over time for each security
+    stockPriceDF = []
+    try:
+        #stockDataSummary is a list of StockPriceSummary objects, documentation for it can be found here: https://docs.intrinio.com/documentation/python/get_security_stock_prices_v2
+        #NOTE: The date will always represent the last day in the period (E.g. End of the week, End of the day)
+        stockDataSummary = security_api.get_security_stock_prices(intrinTicker, start_date=firstIndex, end_date=lastIndex, frequency="daily", page_size=necessaryPageSize)
 
-sucessfulPulls = [["Symbol", "Sector"]]
+    except ApiException as e:
+        print("Exception when calling SecurityApi->get_security_stock_prices: %s\r\n" % e)
+        continue
 
-#if necessaryPageSize != len(stockDataSummary.stock_prices):
-#    print("you goofed")
-
-'''
-print("Stock data first date:" + stockDataSummary.stock_prices[-1].date.strftime("%m/%d/%Y"))
-print("Config keys first date:" + firstIndex.strftime("%m/%d/%Y"))
-print("Stock data last date:" + stockDataSummary.stock_prices[0].date.strftime("%m/%d/%Y"))
-print("Config keys last date:" + lastIndex.strftime("%m/%d/%Y"))
-'''
-#This checks if the data is within our timeframe, there's no need to check if data is empty because of the ApiException above
-if stockDataSummary.stock_prices[-1].date == firstIndex and stockDataSummary.stock_price[0].date == lastIndex:
-    successfulPulls.append(intrinDF.at[1, "Symbol"], intrinDF.at[1, "Sector"])
-
-#accessing one of our stock prices to grab the correct keys for data collection (pop the last because it is a value called "discriminator")
-unformatCols = list(stockDataSummary.stock_prices[0].__dict__.keys())
-unformatCols.pop()
-formatCols = []
-#Formats the columns to be capitalized and omit their first character (which will always be an underscore)
-for columnName in unformatCols:
-    colName = columnName[1:]
-    colName = colName.capitalize()
-    formatCols.append(colName)
-
-#print ("Column Names: " + str(formatCols))
-
-stockPriceDF = []
-if len(stockDataSummary.stock_prices) > 0:
-    stockDSList = [] #stockDSList will hold tuples consisting of data corresponding to the headers of the df
-    for i in range(len(stockDataSummary.stock_prices)):
-        sdp = stockDataSummary.stock_prices[i] #sdp is a Stock Data Point
-        stockDSList.append([sdp.date,sdp.intraperiod,sdp.frequency, sdp.open, sdp.high, sdp.low, sdp.close, sdp.volume, sdp.adj_open, sdp.adj_high, sdp.adj_low, sdp.adj_close, sdp.adj_volume])
-    stockPriceDF = pd.DataFrame(stockDSList, columns = formatCols)
-
-#print ("Data in row 0")
-counter = 0
-'''
-for dataPoint in stockPriceDF.head(10):
-    print (stockPriceDF.iloc[[counter]])
-    counter += 1
-    print()
-'''
-#stockPriceDF.to_csv(intrinDF.at[1, 'Symbol']+"SNDaily.csv")
+    #This checks if the data is within our timeframe, there's no need to check if data is empty because of the ApiException above
+    if stockDataSummary.stock_prices[-1].date == firstIndex and stockDataSummary.stock_price[0].date == lastIndex:
+        successfulPulls.append(intrinTicker, intrinDF.at[tickerRowIndex, "Sector"])
 
 
-#Creating a sucessful file that includes stock tickers and sectors
+    #accessing one of our stock prices to grab the correct keys for data collection (pop the last because it is a value called "discriminator")
+    unformatCols = list(stockDataSummary.stock_prices[0].__dict__.keys())
+    unformatCols.pop()
+    formatCols = []
+    #Formats the columns to be capitalized and omit their first character (which will always be an underscore)
+    for columnName in unformatCols:
+        colName = columnName[1:]
+        colName = colName.capitalize()
+        formatCols.append(colName)
+
+    if len(stockDataSummary.stock_prices) > 0:
+        stockDSList = [] #stockDSList will hold tuples consisting of data corresponding to the headers of the df
+        for i in range(len(stockDataSummary.stock_prices)):
+            sdp = stockDataSummary.stock_prices[i] #sdp is a Stock Data Point
+            stockDSList.append([sdp.date,sdp.intraperiod,sdp.frequency, sdp.open, sdp.high, sdp.low, sdp.close, sdp.volume, sdp.adj_open, sdp.adj_high, sdp.adj_low, sdp.adj_close, sdp.adj_volume])
+        stockPriceDF = pd.DataFrame(stockDSList, columns = formatCols)
+        stockPriceDF.to_csv(os.path.join(Path(configKeys.SN_DATA_FOLDER), intrinTicker+"SNDaily.csv")
+    #sleep takes in an integer representing the number of seconds to pause (we need to pause because of our limited access to API calls)
+    time.sleep(60)
+
+#Creating a successful file that includes stock tickers and sectors
 with open("successfulPullsSN.csv", "w", newline="") as f:
     writer = csv.writer(f)
-    writer.writerows(sucessfulPulls)
+    writer.writerows(successfulPulls)
 '''
 #USE THIS CODE WHEN READING THE CSV's INTO OTHER PYTHON FILES!!!
 '''

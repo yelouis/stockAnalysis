39678a6a5e7c2894ed82e70c8ac4d2c0778d3c33
Louis Ye <yel@carleton.edu>
2020-06-13 18:37:12 -0700

    Fixed spelling error in renaming

---
+++ Pipeline/5_testing.py
@@ -0,0 +1,26 @@
+#This file will keep track of all the moving variables and we can slowly add to that file
+import _configKeys
+
+import pandas as pd
+import collections
+import copy
+import os
+import csv
+import datetime
+from datetime import timedelta
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+import statistics
+from pathlib import Path
+import time
+import numpy as np
+import copy
+import math
+from sklearn.metrics import mean_absolute_error
+
+
+def main():
+    quit()
+
+
+main()
